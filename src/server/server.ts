5210be8fda989a08783117004391344e86018283
Alapan Mukherjee <alapan.mukherjee@futurice.com>
2023-07-14 18:09:22 +0300

    #1 List all clients connected to the server

--- src/server/server.ts
+++ src/server/server.ts
@@ -6,9 +6,25 @@ logger.info(`Server is listening on port ${PORT}`);
 
 io.of('/').on('connect', (socket) => {
   logger.info('A client connected');
+  logger.info(`Number of clients: ${io.of('/').server.engine.clientsCount}`);
 
   socket.on('disconnect', (reason) => {
     logger.info(`A client disconnected. Reason: ${reason}`);
     logger.info(`Number of clients: ${io.of('/').server.engine.clientsCount}`);
+  });
+
+  socket.on('list', (data) => {
+    console.log(data);
+    const users = [];
+
+    for (const [key, value] of io.of('/').sockets) {
+      console.log('Value: ', value)
+      users.push(value.nickname);
+    }
+    socket.emit('list', {
+      sender: data.sender,
+      action: 'list',
+      users
+    });
   });
 });
